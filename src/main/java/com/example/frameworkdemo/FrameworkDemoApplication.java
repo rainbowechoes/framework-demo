8a0542512ffd6c5d3741a99c68ae3b52fa1ae575
yangjian <yj@ragehealth.cn>
2022-07-22 10:52:03 +0800

    fix(security): 添加security控制

--- src/main/java/com/example/frameworkdemo/FrameworkDemoApplication.java
+++ src/main/java/com/example/frameworkdemo/FrameworkDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.frameworkdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import top.rainbowecho.web.servlet.security.EnableSecurity;
 
 @SpringBootApplication
+@EnableSecurity
 public class FrameworkDemoApplication {
 
     public static void main(String[] args) {
